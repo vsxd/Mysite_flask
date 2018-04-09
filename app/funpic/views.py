ebf3bdefcd37d868b534f12c7e6f1e8f2803994e
Xudong <xudong714@gmail.com>
2018-04-09 19:37:06 +0800

    remove Object init in parameter

--- app/funpic/views.py
+++ app/funpic/views.py
@@ -5,7 +5,7 @@ from . import funpic
 from .. import db
 from .forms import Funpic
 from ..decorators import admin_required
-from .spider import LinkSaver
+from .spider import LinkSaver, Downloader, Spider
 
 
 @funpic.route('/disable/<id>')
@@ -37,8 +37,9 @@ def girls():
     form = Funpic()
     query = FunPic.query.filter_by(info='good')
     if form.validate_on_submit():
-        ls = LinkSaver()
-        ls.lazy_init()
+        spider = Spider()
+        downloader = Downloader(spider)
+        ls = LinkSaver(downloader)
         ls.save_to_database()
         redirect(url_for('.girls'))
     pagination = query.order_by(FunPic.timestamp.desc()).paginate(per_page=5)
