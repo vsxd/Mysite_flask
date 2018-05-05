c6a32803eca6426f6f7a4fc6c94134ad859b95f0
Xudong <xudong714@gmail.com>
2018-05-05 21:34:34 +0800

    add funpic.py

---
+++ app/api/funpic.py
@@ -0,0 +1,28 @@
+from flask import jsonify, request, g, url_for, current_app
+from .. import db
+from ..models import FunPic, Permission
+from . import api
+from .decorators import permission_required
+from .errors import forbidden
+
+
+@api.route('/funpic/')
+def get_funny():
+    page = request.args.get('page', 1, type=int)
+    query = FunPic.query.filter_by(info='good').filter_by(type='funny')
+    pagination = query.paginate(
+        page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
+        error_out=False)
+    pics = pagination.items
+    prev = None
+    if pagination.has_prev:
+        prev = url_for('api.get_posts', page=page-1)
+    next = None
+    if pagination.has_next:
+        next = url_for('api.get_posts', page=page+1)
+    return jsonify({
+        'posts': [pic.to_json() for pic in pics],
+        'prev': prev,
+        'next': next,
+        'count': pagination.total
+    })
