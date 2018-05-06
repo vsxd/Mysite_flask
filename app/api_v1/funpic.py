3fd0c2407561e5112fc4fd2570def6c61786322b
Xudong <xudong714@gmail.com>
2018-05-06 17:39:29 +0800

    dev funpic api_v1

--- app/api_v1/funpic.py
+++ app/api_v1/funpic.py
@@ -21,7 +21,29 @@ def get_funny():
     if pagination.has_next:
         next = url_for('api_v1.get_posts', page=page+1)
     return jsonify({
-        'posts': [pic.to_json() for pic in pics],
+        'pics': [pic.to_json() for pic in pics],
+        'prev': prev,
+        'next': next,
+        'count': pagination.total
+    })
+
+
+@api.route('/funpic/girls')
+def get_girls():
+    page = request.args.get('page', 1, type=int)
+    query = FunPic.query.filter_by(info='good').filter_by(type='girls')
+    pagination = query.paginate(
+        page, per_page=current_app.config['PIC_PER_PAGE'],
+        error_out=False)
+    pics = pagination.items
+    prev = None
+    if pagination.has_prev:
+        prev = url_for('api_v1.get_girls', page=page-1)
+    next = None
+    if pagination.has_next:
+        next = url_for('api_v1.get_girls', page=page+1)
+    return jsonify({
+        'pics': [pic.to_json() for pic in pics],
         'prev': prev,
         'next': next,
         'count': pagination.total
